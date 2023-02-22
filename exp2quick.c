ce9c8e96f65249e06e7b6159ff3b3f4bbbefddda
mihikachachad <124241425+mihikachachad@users.noreply.github.com>
2023-02-22 23:00:24 +0530

    EXP2 quick code

---
+++ exp2quick.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+// function to find the partition position
+int partition(int array[], int low, int high) {
+  int pivot = array[high];
+  //int pivot = array[low];
+  //int pivot = array[(high+1)/2];
+  int i = (low - 1);
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    int idx = partition(array, low, high);
+    quickSort(array, low, idx - 1);
+    quickSort(array, idx + 1, high);
+  }
+}
+int main(){
+    FILE *fptr, *sPtr;
+    int index=99;
+    int arrNums[100000];
+    clock_t t;
+    fptr = fopen("Random.txt", "r");
+    sPtr = fopen("iTimes.txt", "w");
+    for(int i=0; i<=999; i++){
+        for(int j=0; j<=index; j++){
+            fscanf(fptr, "%d", &arrNums[j]);
+        }
+        t = clock();
+        quickSort(arrNums, 0, index);
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC;
+        fprintf(sPtr, "%lf\n", time_taken);
+        printf("%lf\n", time_taken);
+        index = index + 100;
+        fseek(fptr, 0, SEEK_SET);
+    }
+    fclose(sPtr);
+    fclose(fptr);
+
+    return 0;
+}
